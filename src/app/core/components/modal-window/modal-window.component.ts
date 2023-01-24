f137805191b0d274cec1dd741d98e870c1c71893
Pavel Sidorovich <pavsid2001@mail.ru>
2023-01-24 21:55:56 +0300

    fix: add Input setter

--- src/app/core/components/modal-window/modal-window.component.ts
+++ src/app/core/components/modal-window/modal-window.component.ts
@@ -13,10 +13,25 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ModalWindowComponent {
-  @Input() public title: string = '';
-  @Input() public body: string = '';
+  @Input() public set title(title: string) {
+    this._title = title;
+  }
+  public get title(): string {
+    return this._title;
+  }
+
+  @Input() public set body(body: string) {
+    this._body = body;
+  }
+  public get body(): string {
+    return this._body;
+  }
+
   @Output() public closeEvent = new EventEmitter();
   @Output() public confirmEvent = new EventEmitter();
+
+  private _title = '';
+  private _body = '';
 
   public onClose(): void {
     this.closeEvent.emit();
