6e77e5321632bfdb09a6c872fc6c1a1c159ab363
Pavel Sidorovich <pavsid2001@mail.ru>
2023-01-25 15:14:29 +0300

    feat: add custom uppercase pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,9 +6,10 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
 import { MaterialModule } from './material.module';
 import { RouterModule } from '@angular/router';
 import { HoverDirective } from './directives/hover.directive';
+import { UppercasePipe } from './pipes/uppercase.pipe';
 
 @NgModule({
-  declarations: [SearchBarComponent, HoverDirective],
+  declarations: [SearchBarComponent, HoverDirective, UppercasePipe],
   imports: [
     CommonModule,
     MaterialModule,
@@ -16,6 +17,6 @@ import { HoverDirective } from './directives/hover.directive';
     RouterModule,
     MaterialModule,
   ],
-  exports: [SearchBarComponent, MaterialModule, HoverDirective],
+  exports: [SearchBarComponent, MaterialModule, HoverDirective, UppercasePipe],
 })
 export class SharedModule {}
